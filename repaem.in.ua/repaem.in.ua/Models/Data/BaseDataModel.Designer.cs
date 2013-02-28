f69702df00df28f826eabbd7f507a719ea4e36e5
anatoliy <tsdaemon@gmail.com>
2013-02-28 16:30:42 +0200

    Initial commit

---
+++ repaem.in.ua/repaem.in.ua/Models/Data/BaseDataModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Tu_chetvekd\Documents\Visual Studio 2010\Projects\repaem.in.ua\repaem.in.ua\Models\Data\BaseDataModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
