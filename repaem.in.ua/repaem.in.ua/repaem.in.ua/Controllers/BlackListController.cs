e1da8c3404e7370c2c576a673596c42ff6843424
Stegniy Anatoliy <tsdaemon@gmail.com>
2013-04-15 23:41:12 +0300

    c

---
+++ repaem.in.ua/repaem.in.ua/repaem.in.ua/Controllers/BlackListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspdev.repaem.Controllers
+{
+    public class BlackListController : Controller
+    {
+        //
+        // GET: /BlackList/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
