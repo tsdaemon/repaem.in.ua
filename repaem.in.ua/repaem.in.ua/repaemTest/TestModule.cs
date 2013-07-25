a04064636c4926492b55cc94768d52d3df18f863
anatoliy <tsdaemon@gmail.com>
2013-07-25 16:33:20 +0300

    test configuration changed

--- repaem.in.ua/repaem.in.ua/repaemTest/TestModule.cs
+++ repaem.in.ua/repaem.in.ua/repaemTest/TestModule.cs
@@ -4,6 +4,7 @@ using Ninject;
 using Ninject.Modules;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
