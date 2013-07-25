d1f9e1f02173bcf0f229dc328cf1ba631d0d0a03
anatoliy <tsdaemon@gmail.com>
2013-07-25 16:59:32 +0300

    Try to get connection string by app.release.config

--- repaem.in.ua/repaem.in.ua/repaemTest/TestModule.cs
+++ repaem.in.ua/repaem.in.ua/repaemTest/TestModule.cs
@@ -14,18 +14,18 @@ namespace repaemTest
     {
         public override void Load()
 	    {
-            if (ConfigurationManager.AppSettings["Environment"] == "Debug") //localhost connection string
-            {
+            //if (ConfigurationManager.AppSettings["Environment"] == "Debug") //localhost connection string
+            //{
                 Bind<IDatabase>().To<Database>().InSingletonScope();
-            }
-            else
-            {
-                var uriString = ConfigurationManager.AppSettings["SQLSERVER_URI"];
-                var uri = new Uri(uriString);
-                SqlConnectionFactory factory = new SqlConnectionFactory(uri.Host, uri.AbsolutePath.Trim('/'), uri.UserInfo.Split(':').First(), uri.UserInfo.Split(':').Last());
+            //}
+            //else
+            //{
+            //    var uriString = ConfigurationManager.AppSettings["SQLSERVER_URI"];
+            //    var uri = new Uri(uriString);
+            //    SqlConnectionFactory factory = new SqlConnectionFactory(uri.Host, uri.AbsolutePath.Trim('/'), uri.UserInfo.Split(':').First(), uri.UserInfo.Split(':').Last());
 
-                Bind<IDatabase>().To<Database>().InSingletonScope().WithConstructorArgument("factory", factory);
-            }
+            //    Bind<IDatabase>().To<Database>().InSingletonScope().WithConstructorArgument("factory", factory);
+            //}
         }
     }
 }
